c35fbff3acfb3d8be5f7b3dd54e937342131ea3c
SeungJun <60959924+seungjun-green@users.noreply.github.com>
2021-07-19 23:05:47 +0900

    Update O(log n).py

--- 50. Pow(x, n)/O(log n).py
+++ 50. Pow(x, n)/O(log n).py
@@ -1,3 +1,16 @@
+"""
+Leet Code Powe(x, n) Solution
+Problem Link: https://leetcode.com/problems/powx-n/
+
+Description: 'Use recursion'
+
+idea: 
+Nobs: x * x * x * x ...... x =  x ^ n
+Pro: x^(n/2) * x^(n/2) = x ^ n
+==> keep calling fastPower(x, n//2) untill we reache the base case
+
+"""
+
 class Solution:
     def myPow(self, x, n: int):
         if n < 0:
@@ -6,6 +19,7 @@ class Solution:
         return self.fastPower(x, n)
     
     def fastPower(self, x, n):
+        #base case
         if (n == 0):
             return 1.0
         

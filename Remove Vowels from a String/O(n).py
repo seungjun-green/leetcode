2297bfcc33c30ef9ec736a06437483189c7f6b52
SeungJun <60959924+seungjun-green@users.noreply.github.com>
2021-07-22 00:36:43 +0900

    Update O(n).py

--- Remove Vowels from a String/O(n).py
+++ Remove Vowels from a String/O(n).py
@@ -1,4 +1,11 @@
+"""
+Leet code Remove Vowels from a String
+Problem Link: https://leetcode.com/problems/remove-vowels-from-a-string/
 
+Solution: 
+Iterate over the given string(s), and if we founds the vowel character we replace that certain 'character' with '',
+and then return the s
+"""
 
 class Solution:
     def removeVowels(self, s):

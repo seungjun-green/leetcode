52f4ec938c08ec87dc5b000e105ae73b20819ef6
SeungJun <60959924+seungjun-green@users.noreply.github.com>
2021-07-22 22:41:52 +0900

    Update O(n).py

--- Two Sums/O(n).py
+++ Two Sums/O(n).py
@@ -1,5 +1,48 @@
 """
 Leet code Two Sums Solution
 Problem Link: https://leetcode.com/problems/two-sum/solution/
-Solution: Check every possible combinaton
+Solution: Use dictionary
+
+
+given list: [2, 7, 11, 15]
+
+make a new dictionary: {2: 0, 7: 1, 11:2, 15: 3}
+                        ^        ^
+                        |        |
+                        Key    Value
+        
+*for key we set the element of the list.
+*for value we set the index number of the element in the list.
+                        
+                        
+ Then start iterating the list(nums) doing following:
+ 
+Get the complement and check whether then search dict whetehr the complement(key exist) 
+ and aslo check whether the 'value of the key'(index in nums) have different value.
+ 
+
+
+
 """
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        
+        #creating a dictionary
+        for index in range(0, len(nums)):
+            dict[nums[index]] = index
+            
+        print(dict)
+            
+        
+        for index in range(0, len(dict)):
+            #get the complement
+            complement = target - nums[index]
+            
+            # check  if complement exist in the dictionary and 
+            # check if the complement has same index as current index(in terms of 'nums' list)
+            if complement in dict and dict[complement] != index:
+                
+                return [index, dict[complement]]
+                        

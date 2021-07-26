09fd3aebae4ba21f973411afe1025948c37f5fef
SeungJun <60959924+seungjun-green@users.noreply.github.com>
2021-07-26 21:55:24 +0900

    Create O(n).py

---
+++ Backspace String Compare/O(n).py
@@ -0,0 +1,32 @@
+class Solution:
+    def backspaceCompare(self, s, t):
+        s = self.reframe(s)
+        t = self.reframe(t)
+        
+        if s == t:
+            return True
+        else:
+            return False
+        
+        
+        
+    def reframe(self, str):
+        new_str = []
+        count = 0
+        for index in range(len(str)):
+            if str[index] == "#":
+                if count < 1:
+                    pass
+                else:
+                    print(index - count)
+                    new_str.pop(count-1)
+                    count -= 1
+         
+            else:
+                new_str.append(str[index])
+                count += 1
+                
+        return ''.join(new_str)
+            
+            
+        

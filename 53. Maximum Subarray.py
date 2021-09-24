cf386aada946980be70e046585627b15a407e377
Seungjun <60959924+seungjun-green@users.noreply.github.com>
2021-09-24 16:47:06 +0900

    Update 53. Maximum Subarray.py

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -1,5 +1,7 @@
 '''
 Time Complexity:  O(n^2)
+
+Algorithm:
 1. Get the sum of all possible sub arrays.
 2. keep updating maxA while getting sum of all possible sub arrays
 3. after end of iteration, return maxA

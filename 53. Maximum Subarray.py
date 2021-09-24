b404917497660515ebd7aca77634fa7344c2abf9
Seungjun <60959924+seungjun-green@users.noreply.github.com>
2021-09-24 16:43:40 +0900

    Create 53. Maximum Subarray.py

---
+++ 53. Maximum Subarray.py
@@ -0,0 +1,17 @@
+'''
+1. Get the sum of all possible sub arrays.
+2. keep updating maxA while getting sum of all possible sub arrays
+3. after end of iteration, return maxA
+'''
+
+class Solution:
+    def maxSubArray(self, nums):
+        maxA = -10000
+        sumA = 0
+        for k in range(1, len(nums)+1):
+            for i in range(len(nums)-k+1):
+                sumA = sum(nums[i:i+k])
+                if sumA > maxA:
+                    maxA = sumA
+                    
+        return maxA

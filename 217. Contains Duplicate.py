e6292914e8eaacb2c43bf2fe23742084daf8cdd8
Seungjun <60959924+seungjun-green@users.noreply.github.com>
2021-09-24 16:31:02 +0900

    Create 217. Contains Duplicate.py

---
+++ 217. Contains Duplicate.py
@@ -0,0 +1,18 @@
+'''
+Time Complexity: O(n)
+
+Algorithms
+1. Convert given list into Sets(which has only distinct values)
+2. Compare length of set and list, if they are same return False, otherwise retrun True
+(because difference of those two length means, the list contains duplicate)
+
+'''
+
+class Solution:
+    def containsDuplicate(self, nums):
+        set1 = set(nums)
+        
+        if len(set1) != len(nums):
+            return True
+        else:
+            return False

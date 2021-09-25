050ec495ffe2def2b40fef65664dd4e6565df55e
Seungjun <60959924+seungjun-green@users.noreply.github.com>
2021-09-25 13:01:24 +0900

    Update 88. Merge Sorted Array.py

--- 88. Merge Sorted Array.py
+++ 88. Merge Sorted Array.py
@@ -17,9 +17,25 @@ class Solution:
         
         
 # Solution - 2/2
-"""
-Time Complexity: O((n+m))
+'''
+Time Complexity: O(n+m)
 
-Algorithm
+Alogrithm:
+1. copy nums1[:m] to nums1Copy
+2. comparing one element in nums1Copy and one element nums2 left to right, input smaller one to nums1. 
+'''
 
-"""
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        nums1Copy = nums1[:m]
+        pr1 = 0
+        pr2 = 0
+        pw = 0
+        
+        for pw in range(n+m):
+            if pr2 >= n or (pr1 < m and nums1Copy[pr1] <= nums2[pr2]):
+                nums1[pw] = nums1Copy[pr1]
+                pr1+=1
+            else:
+                nums1[pw] = nums2[pr2]
+                pr2 += 1

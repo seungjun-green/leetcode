862e6e6be8c837a18daa58f626101e041b4faab7
Seungjun <60959924+seungjun-green@users.noreply.github.com>
2021-09-25 11:42:47 +0900

    Create 88. Merge Sorted Array.py

---
+++ 88. Merge Sorted Array.py
@@ -0,0 +1,20 @@
+
+# Solution - 1/2
+"""
+Time Complexity: O((n+m) * log(n+m))
+- The cost of sorting a list of length xx using a built-in sorting algorithm is O(x * logx )
+
+Algorithm
+1. Merge two list
+2. the sort the merged list
+"""
+
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        for i in range(n):
+            nums1[i + m] = nums2[i]
+        nums1.sort()
+        
+        

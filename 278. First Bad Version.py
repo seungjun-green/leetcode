a55802858e95ee3f51bbb032363f849cfbe948e5
Seungjun <60959924+seungjun-green@users.noreply.github.com>
2021-09-26 22:11:58 +0900

    Update 278. First Bad Version.py

--- 278. First Bad Version.py
+++ 278. First Bad Version.py
@@ -9,3 +9,22 @@
         for i in range(1, n+1):
             if isBadVersion(i):
                 return i
+
+# Solution 2
+ '''
+ Time Complexity: O(log n)
+ 
+ Algorithm: Binary Search
+ '''
+ 
+ class Solution:
+    def firstBadVersion(self, n):
+        start = 1
+        end = n
+        while start < end:
+            mid = (start+end)//2
+            if isBadVersion(mid):
+                end = mid
+            else:
+                start = mid + 1
+        return start

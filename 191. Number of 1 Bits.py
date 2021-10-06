bda0face8c33503efcb306ec1a3c03f913c65e46
Seungjun <60959924+seungjun-green@users.noreply.github.com>
2021-10-06 17:40:58 +0900

    Update 191. Number of 1 Bits.py

--- 191. Number of 1 Bits.py
+++ 191. Number of 1 Bits.py
@@ -1,3 +1,5 @@
+
+# Solution 1 - Brute Force
 class Solution:
     def hammingWeight(self, n: int) -> int:
         count = 1
@@ -11,4 +13,16 @@ class Solution:
             
             n = n // 2
 
+        return count
+    
+    
+    
+ # Solution 2 - Bit Manipulation  
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n != 0:
+            n = n & (n-1)
+            count += 1
+        
         return count

483eb86ff7e136dd825144f69aea8f5a482e0de5
Seungjun <60959924+seungjun-green@users.noreply.github.com>
2021-10-06 20:11:33 +0900

    Update 231. Power of Two.py

--- 231. Power of Two.py
+++ 231. Power of Two.py
@@ -13,3 +13,29 @@ class Solution:
                 n = n // 2       
                 
         return True
+    
+    
+    
+ 
+# Solution 2 - Bit Manipulation
+
+
+'''
+
+2^0 = 1
+2^1 = 10
+2^2 = 100
+2^3 = 1000
+
+
+x & (-x) ==> keep the right most 1-bit and set all other bits to 0
+[computer use 2's complement]
+
+'''
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 0:
+            return False
+        else:
+            return (n & (-n)) == n

6ab818376357467163d7292d01b3da3c60b3f987
Seungjun <60959924+seungjun-green@users.noreply.github.com>
2021-10-27 00:04:55 +0900

    Update 560 Subarray Sum Equals K.py

--- 560 Subarray Sum Equals K.py
+++ 560 Subarray Sum Equals K.py
@@ -1,14 +1,18 @@
 # Solution 1 - Brute Force
 
+'''
+Algorithm:
+1. Create every possible sub array and check whether it's sum is equals to target sum,  if it does increment 'result' variable by 1
+2. after iteration return the value of result variable
+'''
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         result = 0
-        for i in range(len(nums)):
-            tempSum = nums[i]
-            if tempSum == k:
-                result += 1
-            
-            for j in range(i-1,-1,-1):
-                tempSum += nums[i]
-                if tempSum == k:
+        for i in range(0,len(nums)):
+            for j in range(i, len(nums)):
+                temp = nums[i:j+1]
+                current_sum = sum(temp)
+                if current_sum == k:
                     result += 1
+                
+        return result

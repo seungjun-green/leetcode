d858bb0611dda7a77c269589aa79720bd9019172
Seungjun <60959924+seungjun-green@users.noreply.github.com>
2021-10-28 12:03:37 +0900

    Update 704. Binary Search.py

--- 704. Binary Search.py
+++ 704. Binary Search.py
@@ -1,3 +1,5 @@
+# Solution 1 - using iteration
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         start = 0
@@ -11,3 +13,24 @@ class Solution:
             else:
                 start = pivot + 1
         return -1
+
+    
+    
+# Solution 2c-cusing recursion
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        return self.binarySearch(nums,0,len(nums)-1,target)
+        
+    
+    def binarySearch(self, nums, start,end, target):
+        pivot = (start + end) //2
+        if nums[pivot] == target:
+            return pivot
+        elif start >= end and nums[pivot] != target:
+            return -1
+        else:
+            if nums[pivot] < target:
+                return self.binarySearch(nums,pivot+1,end,target)   
+            elif nums[pivot] > target:
+                return self.binarySearch(nums,start,pivot-1,target)

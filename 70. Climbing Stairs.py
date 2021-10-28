e3f1b9d14304720a77fa16ea623354609471b0e3
Seungjun <60959924+seungjun-green@users.noreply.github.com>
2021-10-28 21:36:30 +0900

    Update 70. Climbing Stairs.py

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -1,43 +1,20 @@
-# Solution 1 - Brute Force
-
-
 '''
 Algorithm:
-1. get sum of all possible combination of 1 and 2
+dp[i] = dp[i-1] + dp[i-2]
 '''
 
+# Solution 1 - using recursion
 class Solution:
     def climbStairs(self, n: int) -> int:
-        k = n//2
-        res = 0
-        print("k", k)
-        for i in range(k+1):
-            temp = n - i*2
-            res += self.factorial(i+temp) // (self.factorial(i) * self.factorial(temp))
-            
-            
-            
-        return res
-            
-    
-    def factorial(self, n: int) -> int:
-        if n == 0:
-            return 1
         if n == 1:
             return 1
+        if n == 2:
+            return 2
         else:
-            return n * factorial(n-1)
+            return self.climbStairs(n-1) + self.climbStairs(n-2)
 
 
-# Solution 2 - Dynamic Programming
-
-'''
-Algorithm:
-
-dp[i] = dp[i-1] + dp[i-2]
-
-'''
-
+# Soltion 2 0 iterative way
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1:
@@ -48,8 +25,6 @@ class Solution:
         dp[2] = 2
         
         for i in range(3, n+1):
-            print(dp[i-1], dp[i-2])
             dp[i] = dp[i-1] + dp[i-2]
-        
         
         return dp[n]

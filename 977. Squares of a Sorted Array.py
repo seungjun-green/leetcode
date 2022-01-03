b3a1616cdf711fe068f8c622210047d5d440dce8
Seungjun <60959924+seungjun-green@users.noreply.github.com>
2022-01-03 22:55:14 +0900

    Update 977. Squares of a Sorted Array.py

--- 977. Squares of a Sorted Array.py
+++ 977. Squares of a Sorted Array.py
@@ -1,6 +1,9 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
+        # square every element
         for i in range(len(nums)):
-            nums[i] = nums[i] ** 2    
+            nums[i] = nums[i]*nums[i]
+        
+        # sort the array
         nums.sort()
         return nums
